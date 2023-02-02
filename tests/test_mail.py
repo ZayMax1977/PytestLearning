8a0ea55f8e667932ee6253cce807ea7cc71fbac2
Max Zanchenko <zamax77@mail.ru>
2023-02-02 17:37:08 +0300

    Вход в систему -> Отправка первого письма -> Отправка второго письма -> yeild

--- tests/test_mail.py
+++ tests/test_mail.py
@@ -3,6 +3,8 @@ import pytest
 @pytest.fixture()
 def system_intering():
     print('Вход в систему выполнен')
+    yield
+    print('Выход из системы')
 
 def test_send_mail_1(system_intering):
     print('Письмо 1 отправлено')
